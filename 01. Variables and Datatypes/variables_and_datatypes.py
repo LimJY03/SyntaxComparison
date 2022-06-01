5182a23057290b17b03afb96c5fb4ba3cd64a683
DesigneR- <limjunyi03@gmail.com>
2022-06-01 12:01:33 +0800

    Corrected some syntax errors

--- 01. Variables and Datatypes/variables_and_datatypes.py
+++ 01. Variables and Datatypes/variables_and_datatypes.py
@@ -1,16 +1,16 @@
 # Declare and initialise variables
-fav_number = 10;
-decimal = 2.718;
-fav_alphabet = 'A';
-fav_message = 'Hello World';
-is_married = False;
+fav_number = 10
+decimal = 2.718
+fav_alphabet = 'A'
+fav_message = 'Hello World'
+is_married = False
 
-PI = 3.141592653;
+PI = 3.141592653
 
 # Output variables
-print("My favourite number is " + str(fav_number) + ".");   # My favourite number is 10.
-print(str(decimal) + " is a decimal number.");              # 2.718 is a decimal number.
-print("My favourite alphabet is " + fav_alphabet + ".");    # My favourite alphabet is A.
-print("My favourite message is " + fav_message + ".");      # My favourite message is Hello World.
-print("Am I married? " + str(is_married) + ".");            # Am I married? False.
-print("In math, pi has the value of " + str(PI) + ".");     # In math, pi has the value of 3.141592653.
+print("My favourite number is " + str(fav_number) + ".")    # My favourite number is 10.
+print(str(decimal) + " is a decimal number.")               # 2.718 is a decimal number.
+print("My favourite alphabet is " + fav_alphabet + ".")     # My favourite alphabet is A.
+print("My favourite message is " + fav_message + ".")       # My favourite message is Hello World.
+print("Am I married? " + str(is_married) + ".")             # Am I married? False.
+print("In math, pi has the value of " + str(PI) + ".")      # In math, pi has the value of 3.141592653.
