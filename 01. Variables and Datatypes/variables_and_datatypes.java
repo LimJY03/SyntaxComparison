7bf51c50633b2afa6e9fb04fd900e38b15c01c32
DesigneR- <limjunyi03@gmail.com>
2022-06-01 11:57:33 +0800

    Added Sample Code in Java

---
+++ 01. Variables and Datatypes/variables_and_datatypes.java
@@ -0,0 +1,22 @@
+class variables_and_datatypes{
+
+    public static void main(String[] args){
+
+        // Declare and initialise variables
+        int fav_number = 10;
+        double decimal = 2.718;
+        char fav_alphabet = 'A';
+        String fav_message = "Hello World";
+        boolean is_married = false;
+
+        final double PI = 3.141592653;
+        
+        // Output variables
+        System.out.println("My favourite number is " + fav_number + ".");       // My favourite number is 10.
+        System.out.println(decimal + " is a decimal number.");                  // 2.718 is a decimal number.
+        System.out.println("My favourite alphabet is " + fav_alphabet + ".");   // My favourite alphabet is A.
+        System.out.println("My favourite message is " + fav_message + ".");     // My favourite message is Hello World.
+        System.out.println("Am I married? " + is_married + ".");                // Am I married? false.
+        System.out.println("In math, pi has the value of " + PI + ".");         // In math, pi has the value of 3.141592653.
+    }
+}
