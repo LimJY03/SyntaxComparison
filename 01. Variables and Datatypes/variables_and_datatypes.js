fa4ba934e9860aa69bdfd065b0bf354ceddd0ae4
DesigneR- <limjunyi03@gmail.com>
2022-06-01 12:01:46 +0800

    Added Sample Code in JavaScript

---
+++ 01. Variables and Datatypes/variables_and_datatypes.js
@@ -0,0 +1,16 @@
+// Declare and initialise variables
+var fav_number = 10
+var decimal = 2.718
+var fav_alphabet = 'A'
+var fav_message = 'Hello World'
+var is_married = false
+
+const PI = 3.141592653
+
+// Output variables
+console.log("My favourite number is " + fav_number + ".")       // My favourite number is 10.
+console.log(decimal + " is a decimal number.")                  // 2.718 is a decimal number.
+console.log("My favourite alphabet is " + fav_alphabet + ".")   // My favourite alphabet is A.
+console.log("My favourite message is " + fav_message + ".")     // My favourite message is Hello World.
+console.log("Am I married? " + is_married + ".")                // Am I married? false.
+console.log("In math, pi has the value of " + PI + ".")         // In math, pi has the value of 3.141592653.
