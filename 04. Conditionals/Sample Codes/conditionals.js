9f51e5d473e6b69f681e4a52424cf3d9722fbe5b
DesigneR- <limjunyi03@gmail.com>
2022-06-06 14:21:07 +0800

    Added Sample Code in JavaScript

---
+++ 04. Conditionals/Sample Codes/conditionals.js
@@ -0,0 +1,27 @@
+var marks = 85
+var grade = 'Z'
+
+if(marks >= 0 && marks < 25){
+    grade = 'F'
+}
+else if(marks >= 25 && marks < 45){
+    grade = 'E'
+}
+else if(marks >= 45 && marks < 50){
+    grade = 'D'
+}
+else if(marks >= 50 && marks < 60){
+    grade = 'C'
+}
+else if(marks >= 60 && marks < 80){
+    grade = 'B'
+}
+else if(marks >= 80){
+    grade = 'A'
+}
+else{
+    console.log("Invalid mark!")
+    process.exit(0)                     // Terminates Program
+}
+
+console.log("Grade is: " + grade)       // Grade is: A
