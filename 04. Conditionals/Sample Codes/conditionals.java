8df18c7f3be45714f854757cb73041a4e4f9c610
DesigneR- <limjunyi03@gmail.com>
2022-06-06 14:21:21 +0800

    Added Sample Code in Java

---
+++ 04. Conditionals/Sample Codes/conditionals.java
@@ -0,0 +1,44 @@
+/* 
+ * A school has following rules for grading system:
+ * Below 25 excluded - F
+ * 25 to 45 excluded - E
+ * 45 to 50 excluded - D
+ * 50 to 60 excluded - C
+ * 60 to 80 excluded - B
+ * Above 80 included - A
+ * Display grade based on marks.
+ */
+
+class conditionals{
+
+    public static void main(String[] args){
+
+        double marks = 85;
+        char grade = 'Z';
+
+        if(marks >= 0 && marks < 25){
+            grade = 'F';
+        }
+        else if(marks >= 25 && marks < 45){
+            grade = 'E';
+        }
+        else if(marks >= 45 && marks < 50){
+            grade = 'D';
+        }
+        else if(marks >= 50 && marks < 60){
+            grade = 'C';
+        }
+        else if(marks >= 60 && marks < 80){
+            grade = 'B';
+        }
+        else if(marks >= 80){
+            grade = 'A';
+        }
+        else{
+            System.out.println("Invalid mark!");
+            System.exit(0); // Terminates Program
+        }
+
+        System.out.println("Grade is: " + grade);       // Grade is: A
+    }
+}
